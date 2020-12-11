1fced78efdfafd67267799b1fa3bbb548f329140
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2020-12-11 14:01:39 +0100

    Adapt script for using fork repos instead of patches Use BinUtils 2.35.1 Use GCC 10.2.0 Newlib 4.0.0

--- scripts/002-gcc-stage1.sh
+++ scripts/002-gcc-stage1.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
-# gcc-3.2.3-stage1.sh by AKuHAK
-# Based on gcc-3.2.2-stage1.sh by Naomi Peori (naomi@peori.ca)
+# 002-gcc-stage1.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
-GCC_VERSION=3.2.3
 ## Download the source code.
-SOURCE=http://ftpmirror.gnu.org/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2
-wget --continue $SOURCE || { exit 1; }
-
-## Unpack the source code.
-echo Decompressing GCC $GCC_VERSION. Please wait.
-rm -Rf gcc-$GCC_VERSION && tar xfj gcc-$GCC_VERSION.tar.bz2 || { exit 1; }
-
-## Enter the source directory and patch the source code.
-cd gcc-$GCC_VERSION || { exit 1; }
-if [ -e ../../patches/gcc-$GCC_VERSION-PS2.patch ]; then
-	cat ../../patches/gcc-$GCC_VERSION-PS2.patch | patch -p1 || { exit 1; }
+REPO_URL="https://github.com/ps2dev/gcc.git"
+REPO_FOLDER="gcc"
+BRANCH_NAME="ee-v10.2.0"
+if test ! -d "$REPO_FOLDER"; then
+	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
+else
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
 fi
+
+TARGET_ALIAS="ee" 
+TARGET="mips64r5900el-ps2-elf"
 
 OSVER=$(uname)
 ## Apple needs to pretend to be linux
 if [ ${OSVER:0:6} == Darwin ]; then
 	TARG_XTRA_OPTS="--build=i386-linux-gnu --host=i386-linux-gnu"
-elif [ ${OSVER:0:10} == MINGW64_NT ]; then
-	TARG_XTRA_OPTS="--build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32"
 else
 	TARG_XTRA_OPTS=""
 fi
 
 ## Determine the maximum number of processes that Make can work with.
-if [ ${OSVER:0:5} == MINGW ]; then
-	PROC_NR=$NUMBER_OF_PROCESSORS
-elif [ ${OSVER:0:6} == Darwin ]; then
-	PROC_NR=$(sysctl -n hw.ncpu)
-else
-	PROC_NR=$(nproc)
-fi
+PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-echo "Building with $PROC_NR jobs"
+## Create and enter the toolchain/build directory
+mkdir build-$TARGET-stage1 && cd build-$TARGET-stage1 || { exit 1; }
 
-## For each target...
-for TARGET in "ee"; do
-	## Create and enter the build directory.
-	mkdir build-$TARGET-stage1 && cd build-$TARGET-stage1 || { exit 1; }
+## Configure the build.
+../configure \
+  --quiet \
+  --prefix="$PS2DEV/$TARGET_ALIAS" \
+  --target="$TARGET" \
+  --enable-languages="c" \
+  --with-float=hard \
+  --with-newlib \
+  --disable-nls \
+  --disable-shared \
+  --disable-libssp \
+  --disable-libmudflap \
+  --disable-threads \
+  --disable-libgomp \
+  --disable-libquadmath \
+  --disable-target-libiberty \
+  --disable-target-zlib \
+  --without-ppl \
+  --without-cloog \
+  --with-headers=no \
+  --disable-libada \
+  --disable-libatomic \
+  --disable-multilib \
+  $TARG_XTRA_OPTS || { exit 1; }
 
-	## Configure the build.
-	if [ ${OSVER:0:6} == Darwin ]; then
-		CC=/usr/bin/gcc CXX=/usr/bin/g++ LD=/usr/bin/ld CFLAGS="-O0 -ansi -Wno-implicit-int -Wno-return-type" ../configure --quiet --prefix="$PS2DEV/$TARGET" --target="$TARGET" --enable-languages="c" --with-newlib --without-headers $TARG_XTRA_OPTS || { exit 1; }
-	else
-		../configure --quiet --prefix="$PS2DEV/$TARGET" --target="$TARGET" --enable-languages="c" --with-newlib --without-headers $TARG_XTRA_OPTS || { exit 1; }
-	fi
-
-	## Compile and install.
-	make --quiet clean && make --quiet -j $PROC_NR CFLAGS="$CFLAGS -O2" LDFLAGS="$LDFLAGS -s" && make --quiet install && make --quiet clean || { exit 1; }
-
-	## Exit the build directory.
-	cd .. || { exit 1; }
-
-	## End target.
-done
+## Compile and install.
+make --quiet -j $PROC_NR clean   || { exit 1; }
+make --quiet -j $PROC_NR all     || { exit 1; }
+make --quiet -j $PROC_NR install || { exit 1; }
+make --quiet -j $PROC_NR clean   || { exit 1; }
--- scripts/004-gcc-stage2.sh
+++ scripts/004-gcc-stage2.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
-# gcc-3.2.3-stage2.sh by uyjulian
-# Based on gcc-3.2.2-stage2.sh by Naomi Peori (naomi@peori.ca)
+# 004-gcc-stage2.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
-GCC_VERSION=3.2.3
 ## Download the source code.
-SOURCE=http://ftpmirror.gnu.org/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2
-wget --continue $SOURCE || { exit 1; }
-
-## Unpack the source code.
-echo Decompressing GCC $GCC_VERSION. Please wait.
-rm -Rf gcc-$GCC_VERSION && tar xfj gcc-$GCC_VERSION.tar.bz2 || { exit 1; }
-
-## Enter the source directory and patch the source code.
-cd gcc-$GCC_VERSION || { exit 1; }
-if [ -e ../../patches/gcc-$GCC_VERSION-PS2.patch ]; then
-	cat ../../patches/gcc-$GCC_VERSION-PS2.patch | patch -p1 || { exit 1; }
+REPO_URL="https://github.com/ps2dev/gcc.git"
+REPO_FOLDER="gcc"
+BRANCH_NAME="ee-v10.2.0"
+if test ! -d "$REPO_FOLDER"; then
+	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
+else
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
 fi
+
+TARGET_ALIAS="ee" 
+TARGET="mips64r5900el-ps2-elf"
 
 OSVER=$(uname)
 ## Apple needs to pretend to be linux
 if [ ${OSVER:0:6} == Darwin ]; then
 	TARG_XTRA_OPTS="--build=i386-linux-gnu --host=i386-linux-gnu --enable-cxx-flags=-G0"
-elif [ ${OSVER:0:10} == MINGW64_NT ]; then
-	TARG_XTRA_OPTS="--build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --enable-cxx-flags=-G0"
 else
 	TARG_XTRA_OPTS="--enable-cxx-flags=-G0"
 fi
 
 ## Determine the maximum number of processes that Make can work with.
-if [ ${OSVER:0:5} == MINGW ]; then
-	PROC_NR=$NUMBER_OF_PROCESSORS
-elif [ ${OSVER:0:6} == Darwin ]; then
-	PROC_NR=$(sysctl -n hw.ncpu)
-else
-	PROC_NR=$(nproc)
-fi
+PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-echo "Building with $PROC_NR jobs"
-
-TARGET="ee"
-## Create and enter the build directory.
+## Create and enter the toolchain/build directory
 mkdir build-$TARGET-stage2 && cd build-$TARGET-stage2 || { exit 1; }
 
 ## Configure the build.
-../configure --quiet --prefix="$PS2DEV/$TARGET" --target="$TARGET" --enable-languages="c,c++" --with-newlib --with-headers="$PS2DEV/$TARGET/$TARGET/include" $TARG_XTRA_OPTS || { exit 1; }
+../configure \
+  --quiet \
+  --prefix="$PS2DEV/$TARGET_ALIAS" \
+  --target="$TARGET" \
+  --enable-languages="c,c++" \
+  --with-float=hard \
+  --with-newlib \
+  --disable-nls \
+  --disable-shared \
+  --disable-libssp \
+  --disable-libmudflap \
+  --disable-threads \
+  --disable-libgomp \
+  --disable-libquadmath \
+  --disable-target-libiberty \
+  --disable-target-zlib \
+  --without-ppl \
+  --without-cloog \
+  --with-headers="$PS2DEV/$TARGET_ALIAS/$TARGET/include" \
+  --disable-libada \
+  --disable-libatomic \
+  --disable-multilib \
+  $TARG_XTRA_OPTS || { exit 1; }
 
 ## Compile and install.
-make --quiet clean && make --quiet -j $PROC_NR CFLAGS="$CFLAGS -O2" LDFLAGS="$LDFLAGS -s" && make --quiet install && make --quiet clean || { exit 1; }
+make --quiet -j $PROC_NR clean   || { exit 1; }
+make --quiet -j $PROC_NR all     || { exit 1; }
+make --quiet -j $PROC_NR install || { exit 1; }
+make --quiet -j $PROC_NR clean   || { exit 1; }
