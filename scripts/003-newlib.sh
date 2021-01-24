0f0e1c4b18c2a350ae39858a164e3cbce6b2fcdc
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2021-01-24 18:15:46 +0100

    Add -O2 to the newlib compilation

--- scripts/003-newlib.sh
+++ scripts/003-newlib.sh
@@ -21,7 +21,7 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 rm -rf build-$TARGET && mkdir build-$TARGET && cd build-$TARGET || { exit 1; }
 
 ## Configure the build.
-CFLAGS_FOR_TARGET="-G0" ../configure --prefix="$PS2DEV/$TARGET_ALIAS" --target="$TARGET" || { exit 1; }
+CFLAGS_FOR_TARGET="-G0 -O2" ../configure --prefix="$PS2DEV/$TARGET_ALIAS" --target="$TARGET" || { exit 1; }
 
 ## Compile and install.
 make --quiet -j $PROC_NR clean          || { exit 1; }
