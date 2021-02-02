affbb80bc32fa6a613a2394ecf0e0445abc75c10
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2021-02-02 12:55:13 +0100

    Update to Binutils 2.36.0

--- scripts/001-binutils.sh
+++ scripts/001-binutils.sh
@@ -5,7 +5,7 @@
 ## Download the source code.
 REPO_URL="https://github.com/ps2dev/binutils-gdb.git"
 REPO_FOLDER="binutils-gdb"
-BRANCH_NAME="ee-v2.35.1"
+BRANCH_NAME="ee-v2.36.0"
 if test ! -d "$REPO_FOLDER"; then
 	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
 else
