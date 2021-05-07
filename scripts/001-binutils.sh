9d377f08f062177ab0bd0139bc00bb15e7afa59c
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2021-05-07 22:23:55 +0300

    Simplified scripts and added MINGW32 action
    Fixed bug when project kept on original branch

--- scripts/001-binutils.sh
+++ scripts/001-binutils.sh
@@ -2,19 +2,29 @@
 # 001-binutils.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
 ## Download the source code.
-## Download the source code.
 REPO_URL="https://github.com/ps2dev/binutils-gdb.git"
 REPO_FOLDER="binutils-gdb"
 BRANCH_NAME="ee-v2.36.0"
 if test ! -d "$REPO_FOLDER"; then
 	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
 else
-	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} && git checkout $BRANCH_NAME || exit 1
 fi
 
 TARGET_ALIAS="ee" 
 TARGET="mips64r5900el-ps2-elf"
 TARG_XTRA_OPTS=""
+OSVER=$(uname)
+
+if [ ${OSVER:0:10} == MINGW64_NT ]; then
+	export lt_cv_sys_max_cmd_len=8000
+	export CC=x86_64-w64-mingw32-gcc
+	TARG_XTRA_OPTS="--host=x86_64-w64-mingw32"
+elif [ ${OSVER:0:10} == MINGW32_NT ]; then
+	export lt_cv_sys_max_cmd_len=8000
+	export CC=i686-w64-mingw32-gcc
+	TARG_XTRA_OPTS="--host=i686-w64-mingw32"
+fi
 
 ## Determine the maximum number of processes that Make can work with.
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
--- scripts/003-newlib.sh
+++ scripts/003-newlib.sh
@@ -8,11 +8,24 @@ BRANCH_NAME="ee-v4.1.0"
 if test ! -d "$REPO_FOLDER"; then
 	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
 else
-	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} && git checkout ${BRANCH_NAME} || exit 1
 fi
 
 TARGET_ALIAS="ee" 
 TARGET="mips64r5900el-ps2-elf"
+TARG_XTRA_OPTS=""
+OSVER=$(uname)
+
+## Apple needs to pretend to be linux
+if [ ${OSVER:0:10} == MINGW64_NT ]; then
+	export lt_cv_sys_max_cmd_len=8000
+	export CC=x86_64-w64-mingw32-gcc
+	TARG_XTRA_OPTS="--host=x86_64-w64-mingw32"
+elif [ ${OSVER:0:10} == MINGW32_NT ]; then
+	export lt_cv_sys_max_cmd_len=8000
+	export CC=i686-w64-mingw32-gcc
+	TARG_XTRA_OPTS="--host=i686-w64-mingw32"
+fi
 
 ## Determine the maximum number of processes that Make can work with.
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
@@ -21,7 +34,10 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 rm -rf build-$TARGET && mkdir build-$TARGET && cd build-$TARGET || { exit 1; }
 
 ## Configure the build.
-CFLAGS_FOR_TARGET="-G0 -O2" ../configure --prefix="$PS2DEV/$TARGET_ALIAS" --target="$TARGET" || { exit 1; }
+CFLAGS_FOR_TARGET="-G0 -O2" ../configure \
+  --prefix="$PS2DEV/$TARGET_ALIAS" \
+  --target="$TARGET" \
+  $TARG_XTRA_OPTS || { exit 1; }
 
 ## Compile and install.
 make --quiet -j $PROC_NR clean          || { exit 1; }
